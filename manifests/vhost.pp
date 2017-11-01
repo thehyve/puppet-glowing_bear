e2fd3b183afab86e19fd00e7b86e0fbba227f647
Gijs Kant <gijs@thehyve.nl>
2017-11-01 20:28:54 +0100

    Add Angular routing rewrite rule to Apache virtual host.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -13,6 +13,20 @@ class glowing_bear::vhost inherits glowing_bear::params {
         docroot       => $app_root,
         docroot_owner => $user,
         docroot_group => $user,
+        rewrites      =>  [
+          {
+            comment      =>  'If an existing asset or directory is requested go to it as it is',
+            rewrite_cond =>  [
+              '%{DOCUMENT_ROOT}%{REQUEST_URI} -f [OR]',
+              '%{DOCUMENT_ROOT}%{REQUEST_URI} -d',
+            ],
+            rewrite_rule =>  ['^ - [L]'],
+          },
+          {
+            comment      =>  'If the requested resource doesn\'t exist, use index.html',
+            rewrite_rule =>  ['^ /index.html'],
+          }
+        ],
         require       => Archive::Nexus[$::glowing_bear::params::app_archive],
     }
 
